9cc275d08ae4439a8b9f5353387eaae17a4402a7
Unknown <mnn264@nyu.edu>
2018-12-01 17:23:39 -0500

    post processing

--- koi/Assets/Scripts/FollowMouse.cs
+++ koi/Assets/Scripts/FollowMouse.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class FollowMouse : MonoBehaviour {
 
     public float moveSpeed = 0.004f;
+    Camera cam;
+    Transform reticle;
 
     // Use this for initialization
     void Start () {
+
+        reticle = GameObject.Find("reticle").transform;
+        Cursor.visible = false;
+        cam = Camera.main;
 
     }
   
   // Update is called once per frame
   void FixedUpdate () {
 
-        transform.position = Vector2.Lerp(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition), moveSpeed);
-        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        Vector2 mouseDir = (cam.ScreenToWorldPoint(Input.mousePosition) - transform.position).normalized;
+        reticle.position = Vector2.Lerp(transform.position, cam.ScreenToWorldPoint(Input.mousePosition), moveSpeed) + mouseDir * 2;
+        transform.position = Vector2.Lerp(transform.position, cam.ScreenToWorldPoint(Input.mousePosition), moveSpeed);
+
+        Vector3 difference = cam.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         difference.Normalize();
         float rotation_z = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, rotation_z - 90f);
